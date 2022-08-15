ef9b25a8ca09f5955dc716f331e75c3af5d8a370
Lesslie Houck <Lesslie00@outlook.com>
2022-08-15 11:56:33 -0700

    commit tree 42433e72ba3e4d6a690d395f481bba6a9285abb1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(newArray, event) {
+    let cards = []
+    for ( let i = 0; i < newArray.length; i++ ) {
+      cards.push( `Thank you, ${newArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+  }
+  
+  function countDown(number) {
+    while (number > 0) {
+      console.log(number);
+      number -= 1;
+    }
+    console.log(number);
+  }
